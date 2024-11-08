d1875e78a4ef6402abe8b8c28dac644e905eeed1
Raghupathy <prem.raghupathy@gainwelltechnologies.com>
2024-11-07 16:02:50 -0800

    v1.0.7

--- LambdaAGW/lambda/premTfLmd.py
+++ LambdaAGW/lambda/premTfLmd.py
@@ -1,12 +1,12 @@
 import os
 
 def lambda_handler(event, context):
-    myEnvVar = os.environ.get['TITLE']
+    myEnvVar = os.environ.get('TITLE')
 
     res = {
         "statusCode": 200,
         "headers": {
-            "content-type: application/json"
+            "content-type": "application/json"
         },
         "body":' "Message from " + myEnvVar + "."
     }
